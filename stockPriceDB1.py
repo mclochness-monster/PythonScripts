ef6df0b366256e885fb8c97b5dead431d650bda1
Kevin <kevin.mclaughlin5@gmail.com>
2016-06-23 11:04:18 -0700

    Various python scripts

---
+++ stockPriceDB1.py
@@ -0,0 +1,37 @@
+
+
+import urllib
+import re
+import sqlite3
+
+conn = sqlite3.connect('test1.db')
+print "Opened database successfully";
+
+conn.execute("DROP TABLE IF EXISTS STOCKS")
+
+conn.execute('''CREATE TABLE STOCKS
+	(NAME TEXT PRIMARY KEY	NOT NULL,
+	PRICE TEXT	NOT NULL);''')
+print "Table created successfully";
+
+
+#symbolName = ["aapl","goog","fb","nflx"]
+
+tickerFile = open("tickers.txt")
+
+tickerName = tickerFile.read()
+
+newTickerName = tickerName.split("\n")
+
+i=0
+while i<len(newTickerName) :
+	url = "http://finance.yahoo.com/q?s=" +newTickerName[i] +"&ql=1"
+	htmlfile = urllib.urlopen(url)
+	htmltext = htmlfile.read()
+	regex = '<span id="yfs_l84_'+newTickerName[i] +'">(.+?)</span>'
+	pattern = re.compile(regex)
+	price = re.findall(pattern,htmltext)
+	conn.execute("INSERT INTO STOCKS VALUES (?,?)", (newTickerName[i], price[0]));
+	conn.commit()
+
+	i+=1
